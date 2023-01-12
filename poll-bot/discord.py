3f717cc0c1fdea094e9076b5d7c76538c17c4f57
Ajee <93165947+ajee10x@users.noreply.github.com>
2023-01-12 12:35:46 -0500

    Update discord.py

--- poll-bot/discord.py
+++ poll-bot/discord.py
@@ -14,3 +14,16 @@ async def poll(ctx, *args):
         await message.add_reaction(reaction)
 
 bot.run('YOUR_TOKEN_HERE')
+
+################################
+#         Bot Details
+# DiscordBots/pull-bot 
+# Version: 1.0
+################################
+#         Copy Right Details
+# © OpenBotBook
+# https://github.com/OpenBotBook
+# Apache License
+# Version 2.0, January 2004
+# http://www.apache.org/licenses/
+################################
